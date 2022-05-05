f57efaa6c6aa24560e2ccdb04c055d5af435c038
fabio Barbosa Calvo <unfabio@gmail.com>
2022-05-05 11:04:46 -0500

    add deploy

---
+++ deploy.php
@@ -0,0 +1,13 @@
+<?php 
+function execPrint($command) {
+    $result = array();
+    exec($command, $result);
+    print("<pre>");
+    foreach ($result as $line) {
+        print($line . "\n");
+    }
+    print("</pre>");
+}
+// Print the exec output inside of a pre element
+execPrint("git pull");
+execPrint("git status");
