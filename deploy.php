32dfc98dc76bb6bd21bacf6dd3ed0bb6ba037439
fabio Barbosa Calvo <unfabio@gmail.com>
2022-05-05 11:52:40 -0500

    test deploy

--- deploy.php
+++ deploy.php
@@ -1,4 +1,14 @@
 <?php 
+
+$old_path = getcwd();
+chdir('.');
+//make sure to make the shell file executeable first before running the shell_exec function
+$output = shell_exec('./shell-script.sh');
+chdir($old_path);
+
+echo $output;
+
+/*
 function execPrint($command) {
     $result = array();
     exec($command, $result);
@@ -11,3 +21,4 @@ function execPrint($command) {
 // Print the exec output inside of a pre element
 execPrint("git pull");
 execPrint("git status");
+*/
