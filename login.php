c994b272d0e92421d49154e2216125cf6b400375
fabio Barbosa Calvo <fabio.barbosa.calvo@segurosbolivar.com>
2022-05-20 23:30:59 -0500

    add config

--- login.php
+++ login.php
@@ -1,8 +1,7 @@
 <?php
 # Las claves de acceso, ahorita las ponemos aquí
 # y en otro ejercicio las ponemos en una base de datos
-$usuario_correcto = "kiudata";
-$palabra_secreta_correcta = "kiudata";
+$configs = include('config.php');
 /*
 Para leer los datos que fueron enviados al formulario,
 accedemos al arreglo superglobal llamado $_POST en PHP, y
@@ -14,7 +13,7 @@ $usuario = $_POST["usuario"];
 $palabra_secreta = $_POST["palabra_secreta"];
 
 # Luego de haber obtenido los valores, ya podemos comprobar:
-if ($usuario === $usuario_correcto && $palabra_secreta === $palabra_secreta_correcta) {
+if ($usuario === $configs->username && $palabra_secreta === $configs->password) {
     # Significa que coinciden, así que vamos a guardar algo
     # en el arreglo superglobal $_SESSION, ya que ese arreglo
     # "persiste" a través de todas las páginas
@@ -27,7 +26,7 @@ if ($usuario === $usuario_correcto && $palabra_secreta === $palabra_secreta_corr
     $_SESSION["usuario"] = $usuario;
 
     # Luego redireccionamos a la página "Secreta"
-    header("Location: index.php");
+    header("Location: /");
 } else {
     # No coinciden, así que simplemente imprimimos un
     # mensaje diciendo que es incorrecto
